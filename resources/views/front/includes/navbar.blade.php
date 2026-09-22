a600f6c86bd190dda8d85e8f7faeb468f0af1738
mutasem <mutasemms198@gmail.com>
2026-09-22 15:33:40 +0300

    first commit

--- resources/views/front/includes/navbar.blade.php
+++ resources/views/front/includes/navbar.blade.php
@@ -14,7 +14,9 @@
         @endphp
         <a href="{{ route('home') }}" class="z-brand">
             @if($brandLogo)
-                <img src="{{ asset('assets/uploads/site/' . $brandLogo) }}" alt="{{ $brandName }}" class="z-brand-logo">
+                <span class="z-brand-logo-wrap">
+                    <img src="{{ asset('assets/uploads/site/' . $brandLogo) }}" alt="{{ $brandName }}" class="z-brand-logo">
+                </span>
             @else
                 <span class="z-brand-icon">{{ mb_substr($brandName, 0, 1) }}</span>
                 <span>{{ $brandName }}</span>
